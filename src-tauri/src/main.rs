15159e2c061edafb386469993ceb123291dc91ff
Joao Eduardo Luis <joao@abysmo.io>
2023-01-29 11:48:14 -0100

    ghd: create database on start if doesn't exist
    Signed-off-by: Joao Eduardo Luis <joao@abysmo.io>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,6 +15,8 @@
     windows_subsystem = "windows"
 )]
 
+use sqlx::{migrate::MigrateDatabase, sqlite::SqliteQueryResult, SqlitePool};
+
 mod state;
 
 #[tauri::command]
@@ -26,12 +28,44 @@ fn setup_state() -> state::State {
     state::State::default().init()
 }
 
-fn main() {
+async fn create_db_schema(uri: &str) -> Result<SqliteQueryResult, sqlx::Error> {
+    let pool = SqlitePool::connect(uri).await?;
+    let query = "
+    PRAGMA foreign_keys = ON;
+    CREATE TABLE IF NOT EXISTS settings (
+        key         TEXT PRIMARY KEY NOT NULL,
+        value       TEXT NOT NULL
+    );
+    ";
+
+    let result = sqlx::query(&query).execute(&pool).await;
+    pool.close().await;
+
+    result
+}
+
+async fn setup_db(dbpath: &std::path::PathBuf) {
+    let uri: String = format!("sqlite://{}", dbpath.display());
+    if !sqlx::Sqlite::database_exists(&uri).await.unwrap_or(false) {
+        sqlx::Sqlite::create_database(&uri).await.unwrap();
+        match create_db_schema(&uri).await {
+            Ok(_) => println!("Database created successfully."),
+            Err(err) => panic!("{}", err),
+        };
+    }
+}
+
+#[tokio::main]
+async fn main() {
     let state = setup_state();
 
     println!("  user data dir: {}", state.data_dir.display());
     println!("user config dir: {}", state.config_dir.display());
     println!("  database path: {}", state.db_path.display());
+
+    setup_db(&state.db_path).await;
+
+    tauri::async_runtime::set(tokio::runtime::Handle::current());
 
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
