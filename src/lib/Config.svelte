035243a72f0f9a5e012dfd4385d10534119c9842
Joao Eduardo Luis <joao@abysmo.io>
2023-01-31 14:52:07 -0100

    ghd/ui/config: use bootstrap for forms
    Signed-off-by: Joao Eduardo Luis <joao@abysmo.io>

--- src/lib/Config.svelte
+++ src/lib/Config.svelte
@@ -1,64 +1,56 @@
 <script lang="ts">
-
-    import { invoke } from "@tauri-apps/api/tauri";
+  import { invoke } from "@tauri-apps/api/tauri";
   import { onMount } from "svelte";
+  import { Button, Form, FormGroup, Input, Label } from "sveltestrap";
 
-    let loading = false;
-    let apiToken = "";
-    let isWorking = false;
-    let hasSetToken = false;
-    let isSuccess = false;
+  let loading = false;
+  let apiToken = "";
+  let isWorking = false;
+  let hasSetToken = false;
+  let isSuccess = false;
 
-    onMount(async () => {
-        loading = true;
-        invoke("get_api_token")
-        .then(
-            (res) => {
-                apiToken = <string>res;
-            }
-        )
-        .finally(() => {
-            loading = false;
-        });
-    });
+  onMount(async () => {
+    loading = true;
+    invoke("get_api_token")
+      .then((res) => {
+        apiToken = <string>res;
+      })
+      .finally(() => {
+        loading = false;
+      });
+  });
 
-    async function setToken() {
-        console.log("")
-        invoke("set_api_token", { token: apiToken })
-        .then(
-            (res) => {
-                hasSetToken = true;
-                isSuccess = <boolean>res;
-            }
-        )
-        .finally(() => {
-            isWorking = false;
-        });
-    }
-
+  async function setToken() {
+    console.log("");
+    isWorking = true;
+    invoke("set_api_token", { token: apiToken })
+      .then((res) => {
+        hasSetToken = true;
+        isSuccess = <boolean>res;
+      })
+      .finally(() => {
+        isWorking = false;
+      });
+  }
 </script>
 
-{#if !loading}
-<div>
-    <input
-        id="api-token-input" placeholder="GitHub API Token"
-        bind:value="{apiToken}"
-    />
-    <button on:click="{setToken}">Submit</button>
-    {#if isWorking}
-    <p>Setting value...</p>
-    {:else if hasSetToken}
-        {#if isSuccess}
-            <p>Success setting token!</p>
-        {:else}
-            <p>Error setting token :(</p>
-        {/if}
-    {/if}
-    <p>GitHub API Token: {apiToken}</p>
-
-</div>
+{#if loading}
+  <p>Loading...</p>
 {:else}
-<div>
-    <p>loading...</p>
-</div>
+  <div>
+    <Form>
+      <FormGroup>
+        <Label for="api-token-input">GitHub Token</Label>
+        <Input
+          id="api-token-input"
+          name="apiTokenInput"
+          placeholder="GitHub API Token"
+          bind:value={apiToken}
+        />
+        <Button color="primary" on:click={setToken} disabled={isWorking}
+          >Submit</Button
+        >
+      </FormGroup>
+    </Form>
+  </div>
 {/if}
