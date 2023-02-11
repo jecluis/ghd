1ed6085e09954ae1b6ef234954d15db92b1b5963
Joao Eduardo Luis <joao@abysmo.io>
2023-02-11 14:22:18 -0100

    ghd/ui: listen to tauri events
    Signed-off-by: Joao Eduardo Luis <joao@abysmo.io>

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,68 @@
-import { Component } from "@angular/core";
+// Copyright 2023 Joao Eduardo Luis <joao@abysmo.io>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Component, NgZone, OnDestroy, OnInit } from "@angular/core";
+import {
+  TauriEventListener,
+  TauriListenerEvent,
+  TauriService,
+} from "src/app/shared/services/tauri.service";
 
 @Component({
   selector: "ghd-dashboard",
   templateUrl: "./dashboard.component.html",
   styleUrls: ["./dashboard.component.scss"],
 })
-export class DashboardComponent {}
+export class DashboardComponent
+  implements OnInit, OnDestroy, TauriEventListener
+{
+  public iterationN = 0;
+
+  public constructor(private zone: NgZone, private tauriSvc: TauriService) {}
+
+  public ngOnInit(): void {
+    this.tauriSvc.register(TauriService.events.ITERATION, this);
+  }
+
+  public ngOnDestroy(): void {
+    this.tauriSvc.unregister(TauriService.events.ITERATION, this);
+  }
+
+  public set iteration(value: number) {
+    this.iterationN = value;
+  }
+
+  public get iteration(): number {
+    return this.iterationN;
+  }
+
+  public getListenerID(): string {
+    return "dashboard-evlistener";
+  }
+
+  public handleEvent(event: TauriListenerEvent): void {
+    const evname = event.name;
+
+    if (evname === TauriService.events.ITERATION) {
+      this.handleIteration(<number>event.payload);
+    } else if (evname === TauriService.events.PULL_REQUESTS_UPDATE) {
+    }
+  }
+
+  private handleIteration(n: number) {
+    this.zone.run(() => {
+      this.iterationN = n;
+    });
+  }
+}
---
+++ src/app/shared/services/tauri.service.ts
@@ -0,0 +1,85 @@
+// Copyright 2023 Joao Eduardo Luis <joao@abysmo.io>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Injectable } from "@angular/core";
+import { listen } from "@tauri-apps/api/event";
+
+export type TauriListenerEvent = {
+  name: string;
+  payload: any;
+};
+
+export interface TauriEventListener {
+  handleEvent(event: TauriListenerEvent): void;
+  getListenerID(): string;
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class TauriService {
+  public static events = {
+    ITERATION: "iteration",
+    PULL_REQUESTS_UPDATE: "pull_requests_update",
+  };
+
+  private listeners: Map<string, Map<string, TauriEventListener>>;
+
+  public constructor() {
+    this.listeners = new Map();
+    this.init();
+  }
+
+  private init() {
+    Object.values(TauriService.events).forEach((name: string) => {
+      const evname: string = name;
+      listen(evname, (event) => {
+        const entry: TauriListenerEvent = {
+          name: evname,
+          payload: event.payload,
+        };
+
+        const listenerMap = this.listeners.get(evname);
+        if (!listenerMap) {
+          return;
+        }
+        for (const cb of listenerMap.values()) {
+          cb.handleEvent(entry);
+        }
+      });
+
+      this.listeners.set(evname, new Map());
+    });
+  }
+
+  public register(evname: string, listener: TauriEventListener) {
+    const listenerMap = this.listeners.get(evname);
+    if (!listenerMap) {
+      console.error(`Trying to listen for ${evname}, not tracked.`);
+      return;
+    }
+    const listenerID = listener.getListenerID();
+    listenerMap.set(listenerID, listener);
+  }
+
+  public unregister(evname: string, listener: TauriEventListener) {
+    const listenerMap = this.listeners.get(evname);
+    if (!listenerMap) {
+      console.error(`Trying to unregister for ${evname}, not tracked.`);
+      return;
+    }
+    const listenerID = listener.getListenerID();
+    listenerMap.delete(listenerID);
+  }
+}
